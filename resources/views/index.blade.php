20a705796b9d94675c438d35cc2168f85df6fcda
elyas.bassir <elyas.bassir@gmail.com>
2022-11-17 18:12:05 +0330

    elyas
    bassir

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,6 +13,8 @@
 @include('header_footer.header')
 <br>
 <div class="object_order">
+
+
     <div class="row">
         <div class="col-6 col-sm-4">
             <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="">
@@ -32,120 +34,6 @@
         </div>
     </div>
     <hr>
-    <div class="row">
-        <div class="col-6 col-sm-4">
-            <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="">
-
-        </div>
-        <div class="col-6 col-sm-8 ">
-            <div class="row">
-                <h4 class="title">عنوان</h4>
-                <p class="object_order_detail">
-                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با  گیرد.
-                </p>
-
-            </div>
-            <div class="row object_order_detail_btn">
-                <a href="#" class="btn btn-primary">نمایش بیشتر...</a>
-            </div>
-        </div>
-    </div>
-    <hr>
-    <div class="row">
-        <div class="col-6 col-sm-4">
-            <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="">
-
-        </div>
-        <div class="col-6 col-sm-8 ">
-            <div class="row">
-                <h4 class="title">عنوان</h4>
-                <p class="object_order_detail">
-                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با  گیرد.
-                </p>
-
-            </div>
-            <div class="row object_order_detail_btn">
-                <a href="#" class="btn btn-primary">نمایش بیشتر...</a>
-            </div>
-        </div>
-    </div>
-    <hr>
-    <div class="row">
-        <div class="col-6 col-sm-4">
-            <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="">
-
-        </div>
-        <div class="col-6 col-sm-8 ">
-            <div class="row">
-                <h4 class="title">عنوان</h4>
-                <p class="object_order_detail">
-                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با  گیرد.
-                </p>
-
-            </div>
-            <div class="row object_order_detail_btn">
-                <a href="#" class="btn btn-primary">نمایش بیشتر...</a>
-            </div>
-        </div>
-    </div>
-    <hr><div class="row">
-        <div class="col-6 col-sm-4">
-            <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="">
-
-        </div>
-        <div class="col-6 col-sm-8 ">
-            <div class="row">
-                <h4 class="title">عنوان</h4>
-                <p class="object_order_detail">
-                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با  گیرد.
-                </p>
-
-            </div>
-            <div class="row object_order_detail_btn">
-                <a href="#" class="btn btn-primary">نمایش بیشتر...</a>
-            </div>
-        </div>
-    </div>
-    <hr>
-    <div class="row">
-        <div class="col-6 col-sm-4">
-            <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="">
-
-        </div>
-        <div class="col-6 col-sm-8 ">
-            <div class="row">
-                <h4 class="title">عنوان</h4>
-                <p class="object_order_detail">
-                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با  گیرد.
-                </p>
-
-            </div>
-            <div class="row object_order_detail_btn">
-                <a href="#" class="btn btn-primary">نمایش بیشتر...</a>
-            </div>
-        </div>
-    </div>
-    <hr>
-    <div class="row">
-        <div class="col-6 col-sm-4">
-            <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="">
-
-        </div>
-        <div class="col-6 col-sm-8 ">
-            <div class="row">
-                <h4 class="title">عنوان</h4>
-                <p class="object_order_detail">
-                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با  گیرد.
-                </p>
-
-            </div>
-            <div class="row object_order_detail_btn">
-                <a href="#" class="btn btn-primary">نمایش بیشتر...</a>
-            </div>
-        </div>
-    </div>
-    <hr> 
-
 
 
 </div>
