0179e27fa501da0adb333ffd7e2790535849ed96
elyas.bassir <elyas.bassir@gmail.com>
2022-11-17 19:20:31 +0330

    asddsa
    sdadsa

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@ Route::controller(controller_GET::class)->group(function (){
 
     Route::get('/','index')->name('index');
     Route::get('/login','login_user')->name('login_user');
+Route::get('/show_product/{id}','show_product')->name('show_product');
 
-    
 });
